ee4e33e3e6716624bf30b9f51365d2f1e06550cd
nu12 <34694287+nu12@users.noreply.github.com>
2026-06-09 22:36:05 -0400

    Add new matcher

--- test/matchers_test.rb
+++ test/matchers_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 
 require_relative '../lib/matcher_a'
 require_relative '../lib/matcher_b'
+require_relative '../lib/matcher_c'
 
 class TestMatchers < Minitest::Test
   def test_matcher_a
@@ -22,6 +23,17 @@ class TestMatchers < Minitest::Test
     no_matches = [
       " Total 11 121,35 23 1 335,51"
     ]
+    matches.each { |m| refute_nil matcher.match m }
+    no_matches.each { |nm| assert_nil matcher.match nm }
+  end
+
+  def test_matcher_c
+    matcher = MatcherC.new
+    matches = [
+      " MAY 1 ABC Withdrawal / ABC YYZ 1.25 123.12 321.41",
+      " MAY 7 DEF Transfer - Acc\xC3\xA8sD / from 000000 PPP 12.34 567.89"
+    ]
+    no_matches = []
     matches.each { |m| refute_nil matcher.match m }
     no_matches.each { |nm| assert_nil matcher.match nm }
   end
